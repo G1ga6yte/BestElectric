d83d7e640d05d5cace3d1a1936329a8a90932fec
kristiyan.valchev <krisvalchev@gmail.com>
2024-04-29 20:33:48 +0300

    Added thumbnail for the video before it is loaded

--- src/pages/homeMain/homeHeadBlock/homeMainVideo/homeMainVideo.jsx
+++ src/pages/homeMain/homeHeadBlock/homeMainVideo/homeMainVideo.jsx
@@ -1,62 +1,56 @@
-import React, {useEffect, useRef, useState} from "react";
-import video from  "./Comp-1.webm"
-function HomeMainVideo (){
-  const videoRef = useRef()
+import React, { useEffect, useRef, useState } from "react";
+
+import video from "./Comp-1.webm";
+import videoThumbnail from "./Comp-1-Thumbnail.png";
+
+function HomeMainVideo() {
+  const videoRef = useRef();
   const [videoLoaded, setVideoLoaded] = useState(false);
-  
+
   const handleVideoLoad = () => {
     setVideoLoaded(true);
   };
-  
+
   const handleVideoEnded = (event) => {
     event.target.play();
   };
-  
+
   useEffect(() => {
     const options = {
       root: null,
-      rootMargin: '0px',
-      threshold: 0.1 // Threshold visibility
+      rootMargin: "0px",
+      threshold: 0.6,
     };
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        if (entry.isIntersecting && videoRef.current) { // Add a null check for videoRef.current
-          videoRef.current.play();
-        } else if (videoRef.current) { // Add a null check for videoRef.current
-          videoRef.current.pause();
-        }
+        if (entry.isIntersecting && videoRef.current) videoRef.current.play();
+        else if (videoRef.current) videoRef.current.pause();
       });
     }, options);
 
     observer.observe(videoRef.current);
 
     return () => {
-      if (videoRef.current) {
-        observer.unobserve(videoRef.current);
-      }
+      if (videoRef.current) observer.unobserve(videoRef.current);
     };
   }, []);
-  
-  return(
-     <div className="HomeMainVideo">
-       <video
-          ref={videoRef}
-          src={video}
-          loop
-          preload="auto"
-          style={{ display: videoLoaded ? 'block' : 'none' }}
-          controls={false}
-          onLoadedData={handleVideoLoad}
-          autoPlay
-          muted={true}
-          playsInline={true}
-          onEnded={handleVideoEnded}
-       />
-       
-       
-     </div>
-  )
+
+  return (
+    <div className="HomeMainVideo">
+      <video
+        ref={videoRef}
+        src={video}
+        loop
+        onLoadedData={handleVideoLoad}
+        autoPlay
+        muted={true}
+        playsInline={true}
+        onEnded={handleVideoEnded}
+      />
+      {!videoLoaded && <img src={videoThumbnail} alt="Thumbnail" />}
+    </div>
+  );
 }
 
-export default HomeMainVideo
+export default HomeMainVideo;
